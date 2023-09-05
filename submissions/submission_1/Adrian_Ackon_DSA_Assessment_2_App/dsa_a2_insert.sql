f99d438df82f1ea304298b17f6e75c1cbe945470
akackon <adrianackon@rocketmail.com>
2023-09-05 14:46:02 +0100

    add entire project together with academic documentation

---
+++ submissions/submission_1/Adrian_Ackon_DSA_Assessment_2_App/dsa_a2_insert.sql
@@ -0,0 +1,86 @@
+-- Employee
+INSERT INTO A2_EMPLOYEE (phone_no, name, address, staff_category, job_type, gender, email, dob) VALUES('1234567890', 'John Smith', '123 Main St', 'Nurse', 'Full-time', 'Male', 'johnsmith@example.com', '03/24/1992');
+INSERT INTO A2_EMPLOYEE (phone_no, name, address, staff_category, job_type, gender, email, dob) VALUES('2345678901', 'Jane Doe', '456 Oak Ave', 'Doctor', 'Part-time', 'Female', 'janedoe@example.com', '03/24/1994');
+INSERT INTO A2_EMPLOYEE (phone_no, name, address, staff_category, job_type, gender, email, dob) VALUES('3456789012', 'Robert Johnson', '789 Maple St', 'Administrative Staff', 'Full-time', 'Male', 'robertjohnson@example.com', '03/24/1992');
+INSERT INTO A2_EMPLOYEE (phone_no, name, address, staff_category, job_type, gender, email, dob) VALUES('4567890123', 'Emily Brown', '321 Elm St', 'Nurse', 'Part-time', 'Female', 'emilybrown@example.com', '03/24/1994');
+INSERT INTO A2_EMPLOYEE (phone_no, name, address, staff_category, job_type, gender, email, dob) VALUES('5678901234', 'Michael Lee', '654 Pine Ave', 'Doctor', 'Full-time', 'Male', 'michaellee@example.com', '03/24/1998');
+INSERT INTO A2_EMPLOYEE (phone_no, name, address, staff_category, job_type, gender, email, dob) VALUES('1234567890', 'Jordan Lee', '123 Main St', 'Ancillary Staff', 'Full-time', 'Male', 'jordanlee@example.com', '03/24/2002');
+INSERT INTO A2_EMPLOYEE (phone_no, name, address, staff_category, job_type, gender, email, dob) VALUES('2345678901', 'Harry Potter', '456 Oak Ave', 'Porter', 'Part-time', 'Male', 'harrypotter@example.com', '03/24/1998');
+INSERT INTO A2_EMPLOYEE (phone_no, name, address, staff_category, job_type, gender, email, dob) VALUES('3456789012', 'Fred Weasley', '789 Maple St', 'Porter', 'Full-time', 'Male', 'fredweasley@example.com', '03/24/2002');
+INSERT INTO A2_EMPLOYEE (phone_no, name, address, staff_category, job_type, gender, email, dob) VALUES('4567890123', 'Poppy Pomfrey', '321 Elm St', 'Porter', 'Part-time', 'Female', 'poppypomfrey@example.com', '03/24/1987');
+INSERT INTO A2_EMPLOYEE (phone_no, name, address, staff_category, job_type, gender, email, dob) VALUES('5678901234', 'Fenrir Greyback', '654 Pine Ave', 'Cleaner', 'Full-time', 'Male', 'fenrirgreyback@example.com', '03/24/1987');
+INSERT INTO A2_EMPLOYEE (phone_no, name, address, staff_category, job_type, gender, email, dob) VALUES('1234567890', 'Percy Jackson', '123 Main St', 'Nurse', 'Full-time', 'Male', 'percyjackson@example.com', '03/24/1987');
+INSERT INTO A2_EMPLOYEE (phone_no, name, address, staff_category, job_type, gender, email, dob) VALUES('2345678901', 'Annabeth Chase', '456 Oak Ave', 'Doctor', 'Part-time', 'Female', 'annchase@example.com', '03/24/2000');
+INSERT INTO A2_EMPLOYEE (phone_no, name, address, staff_category, job_type, gender, email, dob) VALUES('3456789012', 'Leo Fritz', '789 Maple St', 'Administrative Staff', 'Full-time', 'Male', 'lfritz@example.com', '03/24/2000');
+INSERT INTO A2_EMPLOYEE (phone_no, name, address, staff_category, job_type, gender, email, dob) VALUES('4567890123', 'Rubeus Hagrid', '321 Elm St', 'Nurse', 'Part-time', 'Male', 'rubhag@example.com', '03/24/2000');
+INSERT INTO A2_EMPLOYEE (phone_no, name, address, staff_category, job_type, gender, email, dob) VALUES('5678901234', 'Jason Thunder', '654 Pine Ave', 'Medical Director', 'Full-time', 'Male', 'michaellee@example.com', '03/24/2005');
+INSERT INTO A2_EMPLOYEE (phone_no, name, address, staff_category, job_type, gender, email, dob) VALUES('1234567890', 'Luke Shaw', '123 Main St', 'Cleaner', 'Full-time', 'Male', 'lshaw@example.com', '03/24/2005');
+INSERT INTO A2_EMPLOYEE (phone_no, name, address, staff_category, job_type, gender, email, dob) VALUES('2345678901', 'Ronald Grint', '456 Oak Ave', 'Ancillary Staff', 'Part-time', 'Male', 'rgrint@example.com', '03/24/2005');
+INSERT INTO A2_EMPLOYEE (phone_no, name, address, staff_category, job_type, gender, email, dob) VALUES('3456789012', 'Percy Wilfre', '789 Maple St', 'Porter', 'Full-time', 'Male', 'pwilfre11@example.com', '03/24/1999');
+INSERT INTO A2_EMPLOYEE (phone_no, name, address, staff_category, job_type, gender, email, dob) VALUES('4567890123', 'Mandrake McGill', '321 Elm St', 'Porter', 'Part-time', 'Female', 'mmgill@example.com', '03/24/1999');
+INSERT INTO A2_EMPLOYEE (phone_no, name, address, staff_category, job_type, gender, email, dob) VALUES('5678901234', 'Justin Mall', '654 Pine Ave', 'Ancillary Staff', 'Full-time', 'Male', 'jmall@example.com', '03/24/1999');
+
+-- Salaried Staff
+INSERT INTO A2_SALARIED_STAFF (emp_id, holiday_rights, pension_rights, salary) VALUES (1, '25', 'Yes', 40000);
+INSERT INTO A2_SALARIED_STAFF (emp_id, holiday_rights, pension_rights, salary) VALUES (2, '25', 'Yes', 63000);
+INSERT INTO A2_SALARIED_STAFF (emp_id, holiday_rights, pension_rights, salary) VALUES (3, '25', 'Yes', 35000);
+INSERT INTO A2_SALARIED_STAFF (emp_id, holiday_rights, pension_rights, salary) VALUES (4, '22', 'No', 58000);
+INSERT INTO A2_SALARIED_STAFF (emp_id, holiday_rights, pension_rights, salary) VALUES (5, '15', 'Yes', 55000);
+INSERT INTO A2_SALARIED_STAFF (emp_id, holiday_rights, pension_rights, salary) VALUES (11, '23', 'Yes', 40000);
+INSERT INTO A2_SALARIED_STAFF (emp_id, holiday_rights, pension_rights, salary) VALUES (12, '20', 'Yes', 61000);
+INSERT INTO A2_SALARIED_STAFF (emp_id, holiday_rights, pension_rights, salary) VALUES (13, '18', 'Yes', 35000);
+INSERT INTO A2_SALARIED_STAFF (emp_id, holiday_rights, pension_rights, salary) VALUES (14, '22', 'No', 55000);
+INSERT INTO A2_SALARIED_STAFF (emp_id, holiday_rights, pension_rights, salary) VALUES (15, '15', 'Yes', 51000);
+
+
+
+-- Hourly Staff
+INSERT INTO A2_HOURLY_STAFF (emp_id, holiday_right, pension_rights, hourly_rates) VALUES (6, '12', 'Yes', 54);
+INSERT INTO A2_HOURLY_STAFF (emp_id, holiday_right, pension_rights, hourly_rates) VALUES (7, '8', 'No', 66);
+INSERT INTO A2_HOURLY_STAFF (emp_id, holiday_right, pension_rights, hourly_rates) VALUES (8, '10', 'Yes', 50);
+INSERT INTO A2_HOURLY_STAFF (emp_id, holiday_right, pension_rights, hourly_rates) VALUES (9, '15', 'Yes', 49);
+INSERT INTO A2_HOURLY_STAFF (emp_id, holiday_right, pension_rights, hourly_rates) VALUES (10, '20', 'Yes', 66);
+INSERT INTO A2_HOURLY_STAFF (emp_id, holiday_right, pension_rights, hourly_rates) VALUES (16, '12', 'Yes', 50);
+INSERT INTO A2_HOURLY_STAFF (emp_id, holiday_right, pension_rights, hourly_rates) VALUES (17, '8', 'No', 60);
+INSERT INTO A2_HOURLY_STAFF (emp_id, holiday_right, pension_rights, hourly_rates) VALUES (18, '10', 'Yes', 55);
+INSERT INTO A2_HOURLY_STAFF (emp_id, holiday_right, pension_rights, hourly_rates) VALUES (19, '15', 'Yes', 45);
+INSERT INTO A2_HOURLY_STAFF (emp_id, holiday_right, pension_rights, hourly_rates) VALUES (20, '20', 'Yes', 65);
+
+-- Doctor
+INSERT INTO A2_DOCTOR (salaried_staff_id, specialization, years_of_experience, license_number, date_of_last_certification, date_of_employment) VALUES (2, 'Cardiologist', 10, '123456', '03/24/2023', '04/24/2020');
+INSERT INTO A2_DOCTOR (salaried_staff_id, specialization, years_of_experience, license_number, date_of_last_certification, date_of_employment) VALUES (5, 'Pediatrician', 5, '234567', '03/24/2023', '03/24/2019');
+INSERT INTO A2_DOCTOR (salaried_staff_id, specialization, years_of_experience, license_number, date_of_last_certification, date_of_employment) VALUES (7, 'Ophthalmologist', 8, '345678', '03/24/2023', '03/24/2019');
+INSERT INTO A2_DOCTOR (salaried_staff_id, specialization, years_of_experience, license_number, date_of_last_certification, date_of_employment, date_of_leaving,reason_for_leaving) VALUES (10, 'Dermatologist', 3, '456789', '03/24/2023', '03/24/2015', '01/24/2023', 'Moving to a new job');
+
+-- Medical Director
+INSERT INTO A2_MEDICAL_DIRECTOR(salaried_staff_id, appointment_date, end_date, doctor_id, date_of_employment) VALUES (10, '01/24/2022', '03/24/2023', 4, '03/24/2015');
+
+-- Nurse
+INSERT INTO A2_NURSE (salaried_staff_id, license_number, nurse_type, years_of_experience, date_of_employment) VALUES (1, 'ABC123', 'RN', 3, '03/24/2015');
+INSERT INTO A2_NURSE (salaried_staff_id, license_number, nurse_type, years_of_experience, date_of_employment) VALUES (4, 'DEF456', 'LPN', 1, '03/24/2015');
+INSERT INTO A2_NURSE (salaried_staff_id, license_number, nurse_type, years_of_experience, date_of_employment) VALUES (6, 'GHI789', 'RN', 5, '03/24/2015');
+INSERT INTO A2_NURSE (salaried_staff_id, license_number, nurse_type, years_of_experience, date_of_employment, date_of_leaving,reason_for_leaving) VALUES (9, 'JKL012', 'LPN', 2, '03/24/2015', '01/24/2023', 'Moving to a new job');
+
+-- Admin staff
+INSERT INTO A2_ADMINISTRATIVE_STAFF (salaried_staff_id, job_title, job_description, department, date_of_employment) VALUES (3, 'Office Manager', 'Responsible for overseeing daily office operations', 'Admin', '03/24/2023');
+INSERT INTO A2_ADMINISTRATIVE_STAFF (salaried_staff_id, job_title, job_description, department, date_of_employment, date_of_leaving,reason_for_leaving) VALUES (8, 'Human Resources Coordinator', 'Responsible for managing employee benefits and payroll', 'HR', '03/24/2016', '01/24/2022', 'Moving to a new job');
+
+-- Ancillary Staff
+INSERT INTO A2_ANCILLARY_STAFF (hourly_staff_id, job_title, department, job_responsibility, date_of_employment) VALUES (1, 'Janitor', 'Facilities', 'Clean and maintain facilities', '03/24/2023');
+INSERT INTO A2_ANCILLARY_STAFF (hourly_staff_id, job_title, department, job_responsibility, date_of_employment) VALUES (7, 'Security Officer', 'Security', 'Patrol premises and ensure safety of staff and patients', '03/24/2023');
+INSERT INTO A2_ANCILLARY_STAFF (hourly_staff_id, job_title, department, job_responsibility, date_of_employment, date_of_leaving,reason_for_leaving) VALUES (10, 'Cook', 'Food Services', 'Prepare meals for patients and staff', '03/24/2020', '01/24/2022', 'Moving to a new job');
+
+-- Porter
+INSERT INTO A2_PORTER(hourly_staff_id, job_title, job_responsibility, date_of_employment) VALUES (2, 'Porter', 'Deliver food trays to patient rooms', '03/24/2023');
+INSERT INTO A2_PORTER(hourly_staff_id, job_title, job_responsibility, date_of_employment) VALUES (3, 'Porter', 'Transport patients to and from their rooms', '03/24/2023');
+INSERT INTO A2_PORTER(hourly_staff_id, job_title, job_responsibility, date_of_employment) VALUES (4, 'Porter', 'Clean and sanitize hospital equipment', '03/24/2023');
+INSERT INTO A2_PORTER(hourly_staff_id, job_title, job_responsibility, date_of_employment) VALUES (8, 'Porter', 'Assist with hospital maintenance tasks', '03/24/2023');
+INSERT INTO A2_PORTER(hourly_staff_id, job_title, job_responsibility, date_of_employment, date_of_leaving,reason_for_leaving) VALUES (9, 'Porter', 'Manage hospital waste disposal', '03/24/2019', '01/24/2021', 'Moving to a new job');
+
+-- Cleaner
+INSERT INTO A2_CLEANER (hourly_staff_id, job_title, job_responsibility, date_of_employment) VALUES (5, 'Janitor', 'Sweep and mop floors, empty trash, clean restrooms','03/24/2023');
+INSERT INTO A2_CLEANER (hourly_staff_id, job_title, job_responsibility, date_of_employment, date_of_leaving,reason_for_leaving) VALUES (6, 'Housekeeper', 'Clean patient rooms, change linens, sanitize surfaces','01/01/2023', '01/24/2023', 'Moving to a new job');
+
+--------------------------------------------------------------
+-- End of DML file
+--------------------------------------------------------------
